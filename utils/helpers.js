25a6efeedf1350fce1bb84e30c61d5d8be3fb95e
WJFil <greg.kitchen7@gmail.com>
2023-03-20 20:51:21 -0600

    "helper.js"

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,6 @@
 module.exports = {
   format_date: (date) => {
+
     // Format date as MM/DD/YYYY
     return date.toLocaleDateString();
   },
@@ -19,4 +20,8 @@ module.exports = {
       return `<span for="img" aria-label="gear">⚙️</span>`;
     }
   },
+
+    return date.toLocaleDateString();
+  }
+
 };

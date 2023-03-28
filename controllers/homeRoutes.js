82253fe4ec8fb570ee9638c172e0821cfc780261
Masyn N <masynnay33@gmail.com>
2023-03-27 20:46:20 -0600

    fixed score route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,65 +3,33 @@ const { User, Dictionary, Scores } = require('../models');
 const session = require('express-session');
 const withAuth = require('../utils/auth');
 
-/*router.get('/', async (req, res) => {
-  try {
-    const DictionaryData = await Dictionary.findAll()
-    const Dictionary = DictionaryData.map((Dictionary) => Dictionary.get({ plain: true }));
-    res.render('homepage', { 
-      Dictionary, 
-      logged_in: req.session.logged_in 
-    });
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});*/
-
-router.get('/', async (req, res) => {
-
-  //const DictionaryData = await Dictionary.findAll()
-  //const Dictionary = DictionaryData.map((Dictionary) => Dictionary.get({ plain: true }));
-  if (req.session.logged_in) {
-    res.render('game', {
-      logged_in: req.session.logged_in 
-    });
-    
-    return;
-  }
-  res.render('homepage', {
-    logged_in: req.session.logged_in 
-  });
- 
-});
-
-router.get('/dictionary', async (req, res) => {
-  try {
-    const DictionaryData = await Dictionary.findByPk(req.params.id, {
-    });
-    const Dictionary = DictionaryData.get({ plain: true });
-    res.render('Dictionary', {
-      ...Dictionary,
-      logged_in: req.session.logged_in
-    });
-  } catch (err) {
-    res.status(400).json(err);
-  }
-});
 
 router.get('/scores', withAuth, async (req, res) => {
   try {
-    const scoreData = await Scores.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
-      include: [{ model: Scores }],
-    });
-    const Score = scoreData.get({ plain: true });
-    res.render('scores', {
-      ...Score,
-      logged_in: true
+    if (req.session.logged_in) {
+      return res.render('scores', {
+        logged_in: req.session.logged_in 
+      });
+    }
+    return res.render('scores', {
+      logged_in: req.session.logged_in 
     });
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
+
+    // const scoreData = await Scores.findByPk(req.session.user_id,
+    //    {
+    //   attributes: { exclude: ['password'] },
+    //   include: [{ model: Scores }],
+    // });
+    // const Score = scoreData.get({ plain: true });
+    // res.render('scores', {
+    //   ...Score,
+    //   logged_in: true
+    // });
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {

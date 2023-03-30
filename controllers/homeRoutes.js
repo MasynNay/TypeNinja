dc67f8ab25069c77a4bf945e5cf97047ca81d345
Masyn N <masynnay33@gmail.com>
2023-03-29 18:33:14 -0600

    removed unecessary lines of code

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -42,35 +42,6 @@ router.get('/scores', async (req, res) => {
   }
 });
 
-
-
-/*code that existed before gk changes 3/28/2023
-
-
-router.get('/scores', withAuth, async (req, res) => {
-  try {
-    if (req.session.logged_in) {
-      return res.render('scores', {
-        logged_in: req.session.logged_in 
-      });
-    }
-  } catch (err) {
-    console.log(err);
-    res.status(500).json(err);
-  }
-});*/
-
-    // const scoreData = await Scores.findByPk(req.session.user_id,
-    //    {
-    //   attributes: { exclude: ['password'] },
-    //   include: [{ model: Scores }],
-    // });
-    // const Score = scoreData.get({ plain: true });
-    // res.render('scores', {
-    //   ...Score,
-    //   logged_in: true
-    // });
-
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/game');
